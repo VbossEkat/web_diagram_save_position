100e395e3988e384c8ef98db5ab9cc0b027f06bf
Yusnel Rojas Garcia <yrojass@gmail.com>
2015-04-25 11:21:48 -0500

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Web Diagram Save Position',
+    'category': 'Tools',
+    'description': """
+Save the position of each node when changed.
+=========================
+
+""",
+    'version': '1.0',
+    'depends': ['web_diagram'],
+    'data' : [
+        'views/backend_assets.xml',
+    ],
+    'auto_install': True,
+}
